4375de9c7aec63d63a78a1861fd1959c61d50470
Arthur Oliveira <arthuroliveiraspm@gmail.com>
2023-08-30 13:59:38 -0300

    fim das aulas assincronas

---
+++ fundamentos/secao-04-javascript-dom-eventos-webstorage/dia-3-eventos/README.js
@@ -0,0 +1,5 @@
+// introdução aos eventos
+// Mudar o conteúdo, o estilo e outros atributos de elementos HTML a partir de eventos do JavaScript;
+
+// Aplicar em seu código JavaScript eventos de click, mouseover, mouseleave, entre outros.
+
