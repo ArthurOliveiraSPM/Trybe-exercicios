05dda8e808e60b01387a898d91a0ca70c963e03b
Arthur Oliveira <arthuroliveiraspm@gmail.com>
2023-10-17 16:37:44 -0300

    entendi

---
+++ front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/04-async-await-e-testes-assincronos/Async e Await/async.js
@@ -0,0 +1,1 @@
+Basicamente o Await irá fazer com que o codigo espere o método ser finalizado para passar para a proxima linha, mas para isso a função precisa ser declarada com assincrona e para fazer isso precisa colocar Async na frente da função.
--- front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/04-async-await-e-testes-assincronos/README.md
+++ front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/04-async-await-e-testes-assincronos/README.md
@@ -1,1 +1,5 @@
  
+1 - Escrever códigos assíncronos utilizando async/await;
+2 - Utilizar o try/catch para tratar erros;
+3 - Escrever testes para códigos assíncronos utilizando async/await.
+
