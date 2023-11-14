a47ffff14a0201bfb54533ffe2f8ba0a66fd69e6
Arthur Oliveira <arthuroliveiraspm@gmail.com>
2023-11-14 14:49:30 -0300

    Fim do dia

--- front-end/secao-04-testes-automatizados-com-React-Testing-Library/01-Introduçao-a-React-Testing-Library/RTL-project/src/App.tsx
+++ front-end/secao-04-testes-automatizados-com-React-Testing-Library/01-Introduçao-a-React-Testing-Library/RTL-project/src/App.tsx
@@ -1,17 +1,41 @@
-// src/App.tsx
+import { useState } from 'react';
 import './App.css';
+import ValidEmail from '../src/components/ValidEmail';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <label htmlFor="id-email">
-        Email
-        <input id="id-email" type="email" />
-      </label>
-      <input data-testid="id-send" type="button" value="Enviar" />
-      <input type="button" value="Voltar" />
-    </div>
-  );
+  const [email, setEmail] = useState('');
+  const [title, setTitle] = useState('')
+
+  function handleClick() {
+        setTitle(email);
+        setEmail('');
+      }
+    
+
+   return (
+     <div className="App">
+       <label htmlFor="id-email">
+         Email:
+         <input
+           id="id-email"
+           type="email" 
+           onChange={ ({target}) => setEmail(target.value)}
+           value={ email }
+         />
+       </label>
+       <input
+         id="btn-send"
+         type="button"
+         data-testid="id-send"
+         value="Enviar"
+         onClick={ handleClick }
+         />
+       <input id="btn-back" type="button" value="Voltar" />
+      <ValidEmail email={ title } />
+     </div>
+   );
 }
 
 export default App;
